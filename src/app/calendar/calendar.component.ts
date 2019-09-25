caa92863fbd0e7b3408af3e94f27310905283a94
Alejandro Segovia Gallardo <asega1996@gmail.com>
2019-09-25 16:58:36 +0200

    Changes Calendear

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataSource } from '@angular/cdk/table';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ConcertService } from './calendar.service';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +12,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  timeStart = {hour: 14, minute: 0};
+  timeEnd = {hour: 20, minute: 0};
+
+  private newConcertVar: Boolean
+
+  constructor(private concertService : ConcertService, private _formBuilder: FormBuilder) { }
+
+  public concerts = [] as Concert[]
+  
+  dataSource = new MatTableDataSource<Concert>(this.concerts);
+
+  concertForm : FormGroup;
+
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+
+  displayedColumns: string[] = ['name', 'dateStart', 'dateEnd', 'options'];
 
   ngOnInit() {
+    this.newConcertVar = false;
+    this.getConcerts()
+    this.dataSource.paginator = this.paginator;
+    this.concertForm = this._formBuilder.group({
+      name: ['',Validators.required],
+      dateStart: ['',Validators.required],
+      dateEnd: ['',Validators.required],
+      timeStart: ['',Validators.required],
+      timeEnd: ['',Validators.required],
+      _id: ['','']
+    })
   }
 
+  newConcert(){
+    this.newConcertVar = (this.newConcertVar)? false : true
+    if(this.newConcertVar) this.concertForm.reset();
+
+  }
+
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  async getConcerts(){
+    this.concerts = await this.concertService.getConcerts();
+    this.dataSource.data = this.concerts;
+  }
+
+  patchValue(element){
+    console.log(element)
+    this.concertForm.patchValue({
+      firstName: element.firstName,
+      lastName: element.lastName,
+      phone: element.phone,
+      instrument: element.instrument,
+      _id: element._id
+    })
+
+    console.log(this.concertForm)
+  }
+
+  async onSubmit(){
+    if (this.concertForm.valid) {
+      await this.concertService.sendFrom(this.concertForm);
+      this.getConcerts();
+    }
+    this.concertForm.reset()
+    this.concertService.getConcerts()
+    this.reloadIrame();
+  }
+
+  async deleteConcert(element){
+    await this.concertService.deleteConcert(element._id);
+    this.getConcerts();
+    this.reloadIrame()
+  }
+
+  reloadIrame() {
+    document.getElementById('googleCalendar').ownerDocument.location.reload(true);
+  }
+
+}
+
+export interface Concert{
+  _id : String,
+  dateStart: Date,
+  dateEnd: Date,
+  name: String
 }
--- src/app/musician/musician.component.ts
+++ src/app/musician/musician.component.ts
@@ -4,33 +4,106 @@ import { MusicianService } from './musician.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { FormGroup, FormControl,Validators, FormBuilder } from '@angular/forms';
 
-let ELEMENT_DATA: Musician[] = [];
+
 
 @Component({
   selector: 'app-musician',
   templateUrl: './musician.component.html',
+  animations: [
+    trigger('EnterLeave', [
+      state('flyIn', style({ transform: 'translateX(0)' })),
+      state('hidden', style({ display: 'none'})),
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.5s 300ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-out', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ],
   styleUrls: ['./musician.component.css']
 })
 export class MusicianComponent implements OnInit {
+  private newMusicianVar: Boolean
 
-  constructor(private musicianService : MusicianService) { }
+  constructor(private musicianService : MusicianService, private _formBuilder: FormBuilder) { }
 
   public musicians = [] as Musician[]
+  public instruments = [] as Instrument[]
   
   dataSource = new MatTableDataSource<Musician>(this.musicians);
 
+  musicianForm : FormGroup;
+
+
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  displayedColumns: string[] = ['firstName', 'lastName', 'instrument', 'phone'];
+
+  displayedColumns: string[] = ['firstName', 'lastName', 'instrument', 'phone', 'options'];
 
   ngOnInit() {
+    this.newMusicianVar = false;
     this.getMusicians()
+    this.dataSource.paginator = this.paginator;
+    this.musicianForm = this._formBuilder.group({
+      firstName: ['',Validators.required],
+      lastName: ['',Validators.required],
+      phone: ['',Validators.required],
+      instrument: ['',Validators.required],
+      _id: ['','']
+    })
+    this.getInstruments();
+  }
+
+  newMusician(){
+    this.newMusicianVar = (this.newMusicianVar)? false : true
+    if(this.newMusicianVar) this.musicianForm.reset();
+
+  }
+
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
   async getMusicians(){
     this.musicians = await this.musicianService.getMusicians();
     this.dataSource.data = this.musicians;
+  }
+
+  async getInstruments(){
+    this.instruments = await this.musicianService.getInstruments();
+  }
+
+  patchValue(element){
+    console.log(element)
+    this.musicianForm.patchValue({
+      firstName: element.firstName,
+      lastName: element.lastName,
+      phone: element.phone,
+      instrument: element.instrument,
+      _id: element._id
+    })
+
+    console.log(this.musicianForm)
+  }
+
+  async onSubmit(){
+    if (this.musicianForm.valid) {
+      await this.musicianService.sendFrom(this.musicianForm);
+      this.getMusicians();
+    }
+    this.musicianForm.reset()
+  }
+
+  async deleteMusician(element){
+    await this.musicianService.deleteMusician(element._id);
+    this.getMusicians();
   }
 
 }
